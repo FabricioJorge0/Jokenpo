29de011bfbaaa76d0338c4f4a255ad3d9420fc0c
Fabricio <fabricioj.dev@gmail.com>
2023-02-25 17:05:38 -0400

    atualizando a forma de imprimir o resultado

--- index.js
+++ index.js
@@ -21,38 +21,49 @@ function gerarValor(min, max){
 
 //FUNÇÃO QUE IRA INICIAR O JOGO AO CLICAR NO BOTAO JOGAR
 function jogar(){
+  var resultado = document.getElementById("resultado")
   const computador = gerarValor(0, 2)
   var jogador = document.querySelector("#jogo")
   const sorteio_pc = ['Pedra', 'Papel', 'Tesoura']
+  resultado.innerHTML = ""
+  resultado.style.color = "white"
 
   switch(jogador.value){
-    case '0': alert(`Voce escolheu: Pedra \nComputador escolheu: ${sorteio_pc[computador]}`)
+    case '0': resultado.innerHTML = "Voce escolheu: Pedra <br> Computador escolheu: " + sorteio_pc[computador]
     break
 
-    case '1': alert(`Voce escolheu: Papel \nComputador escolheu: ${sorteio_pc[computador]}`)
+    case '1': resultado.innerHTML = "Voce escolheu: Papel <br> Computador escolheu: " + sorteio_pc[computador]
     break;
 
-    case '2': alert(`Voce escolheu: Tesoura \nComputador escolheu: ${sorteio_pc[computador]}`)
+    case '2': resultado.innerHTML = "Voce escolheu: Tesoura <br> Computador escolheu: " +  sorteio_pc[computador]
     break
     
-    default: alert("Por favor, escolha uma das opcções")
+    default: resultado.innerHTML = "Por favor, escolha uma das opcções"
 
 
   }
 
   //Condição para decidir onde vai ser adicionado o valor
-  if((computador == 0 && jogador.value == '2') || (computador == 1 && jogador.value == '0') || (computador == 2 && jogador.value == '1')){
-      //COMPUTADOR VENCE
-      adicionar_derrota(contador_derrota+=1)
-      alert("QUE PENA, O COMPUTADOR VENCEU!")
-  }else if(computador == jogador.value){
-          //empate
-      adicionar_empate(contador_empate+=1)
-      alert("POXA, EMPATOU!!")
-  }else{
-      //JOGADOR VENCE
-      adicionar_vitoria(contador_vitoria+=1)
-      alert("PARABÉNS VOCÊ GANHOU!")
-  }
+  setTimeout(() =>{
+      if((computador == 0 && jogador.value == '2') || (computador == 1 && jogador.value == '0') || (computador == 2 && jogador.value == '1')){
+          //COMPUTADOR VENCE
+          adicionar_derrota(contador_derrota+=1)
+          resultado.style.display = "block"
+          resultado.style.color = "red"
+          resultado.innerHTML = "QUE PENA, O COMPUTADOR VENCEU!"
+      }else if(computador == jogador.value){
+              //empate
+          adicionar_empate(contador_empate+=1)
+          resultado.style.display = "block"
+          resultado.style.color = "white"
+          resultado.innerHTML = "POXA, EMPATOU"
+      }else{
+          //JOGADOR VENCE
+          adicionar_vitoria(contador_vitoria+=1)
+          resultado.style.display = "block"
+          resultado.style.color = "green"
+          resultado.innerHTML = "PARABÉNS, VOCÊ GANHOU!!!"
+      }
+  }, 3000)
 }
 
